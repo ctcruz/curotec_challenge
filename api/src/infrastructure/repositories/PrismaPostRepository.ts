2010f60c8dfe39e6bfcd76553cb320c897359a77
Tarsis Cruz <costacruzt@gmail.com>
2025-07-09 09:10:13 -0300

    feat: create PrismaPostRepository

---
+++ api/src/infrastructure/repositories/PrismaPostRepository.ts
@@ -0,0 +1,30 @@
+import { PostRepository } from "../../core/interfaces/repositories/PostRepository";
+import { Post } from "../../core/entities/post.entity";
+import { PrismaClient } from "@prisma/client";
+
+export class PrismaPostRepository implements PostRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async findAll(): Promise<Post[]> {
+    return this.prisma.post.findMany();
+  }
+
+  async update(id: number, post: Partial<Post>): Promise<Post> {
+    return this.prisma.post.update({
+      where: { id },
+      data: post,
+    });
+  }
+
+  delete(id: number): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async create(post: Omit<Post, "id">): Promise<Post> {
+    return this.prisma.post.create({ data: post });
+  }
+
+  async findById(id: number): Promise<Post | null> {
+    return this.prisma.post.findUnique({ where: { id } });
+  }
+}
