2fb9ff8394d0f38214274bf13c3ab71802542b27
Tarsis Cruz <costacruzt@gmail.com>
2025-07-09 08:58:54 -0300

    feat: use middlewares and initialize prisma

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma";
+import { errorHandler } from "./infrastructure/web/middleware/errorHandler";
+import { requestLogger } from "./infrastructure/web/middleware/requestLogger";
 
 // Initial setup
 const app = express();
+const prisma = new PrismaClient();
 
 // Middlewares
 app.use(express.json());
+app.use(requestLogger);
 
-// Server initialization
+// Error Handler Middleware
+app.use(errorHandler);
+
+// Server Initialization
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Graceful Shutdown
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
