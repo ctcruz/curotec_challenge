2727e20d1bfc124f9b28c55609f68fd10b85769c
Tarsis Cruz <costacruzt@gmail.com>
2025-07-09 09:46:13 -0300

    feat: create AuthController

---
+++ api/src/infrastructure/web/controllers/AuthController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { RegisterUseCase } from "../../../core/usecases/RegisterUseCase";
+import { LoginUseCase } from "../../../core/usecases/LoginUseCase";
+
+export class AuthController {
+  constructor(
+    private readonly loginUseCase: LoginUseCase,
+    private readonly registerUseCase: RegisterUseCase
+  ) {}
+
+  async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      const token = await this.loginUseCase.execute(email, password);
+      res.json({ token });
+    } catch (error) {
+      const message = error instanceof Error ? error.message : "Unknown error";
+      res.status(401).json({ error: message });
+    }
+  }
+
+  async register(req: Request, res: Response) {
+    try {
+      const { name, email, password } = req.body;
+      const user = await this.registerUseCase.execute(name, email, password);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : "Unknown error";
+      res.status(400).json({ error: message });
+    }
+  }
+}
