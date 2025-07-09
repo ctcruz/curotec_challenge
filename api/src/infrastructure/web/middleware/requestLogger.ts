b7e70c542eb0c93a55277c745c5989a1f8434475
Tarsis Cruz <costacruzt@gmail.com>
2025-07-09 08:53:54 -0300

    feat: create requestLogger middleware

---
+++ api/src/infrastructure/web/middleware/requestLogger.ts
@@ -0,0 +1,7 @@
+// src/infrastructure/web/middleware/requestLogger.ts
+import { Request, Response, NextFunction } from "express";
+
+export function requestLogger(req: Request, res: Response, next: NextFunction) {
+  console.log(`${req.method} ${req.path}`);
+  next();
+}
