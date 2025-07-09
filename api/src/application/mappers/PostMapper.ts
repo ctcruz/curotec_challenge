0d20249c92f5c18c18c6ab28cbbb2a8e297314c4
Tarsis Cruz <costacruzt@gmail.com>
2025-07-09 09:08:26 -0300

    feat: create PostMapper

---
+++ api/src/application/mappers/PostMapper.ts
@@ -0,0 +1,25 @@
+import { Post } from "../../core/entities/post.entity";
+import { CreatePostRequest } from "../dto/requests/CreatePostRequest.dto";
+import { PostResponse } from "../dto/responses/PostResponse.dto";
+
+export class PostMapper {
+  // Convert request DTO to domain entity
+  static toDomain(dto: CreatePostRequest): Post {
+    return {
+      title: dto.title,
+      content: dto.content,
+      published: dto.published || false,
+    };
+  }
+
+  // Convert domain entity to response DTO
+  static toResponse(post: Post): PostResponse {
+    return new PostResponse(
+      post.id!,
+      post.title,
+      post.content,
+      post.published,
+      post.createdAt!
+    );
+  }
+}
